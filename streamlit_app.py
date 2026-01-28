9cb0eb17c765393310fdbbfdfb652e7caa613c4d
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-01-28 17:14:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -533,8 +533,7 @@ def pg_srv_6():
             avg_df
          with row4[1]:
              avg_df = df_gr.groupby('Type').apply(lambda x: x['Level'].sum() / x['Level'].count(), include_groups=False).to_frame('Level')
-             #avg_df = pd.DataFrame(avg_df, index=['Type','Level'])
-             #build_main_chart(avg_df,"Level Average")
+             # build_main_chart(avg_df,"Level Average")
              avg_df.Type[0]
 
 

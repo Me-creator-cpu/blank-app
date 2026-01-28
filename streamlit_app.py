5d171fc6fc50bc7b6882353ceb5c4278152a202c
Me-creator-cpu <atoussaint_dummy@orange.fr>
2026-01-28 14:27:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -678,34 +678,31 @@ def pg_test_graph():
 def pg_test_tiles():
     total_cells_per_row_or_col = 5
     #titles=[f"Image #{str(i)}" for i in range(len(images))]
-    for i in range(1, (total_cells_per_row_or_col+1)):
+    for i in range(1, (total_cells_per_row_or_col)):
         tlst = ([1] * total_cells_per_row_or_col) + [2] # 2 = rt side padding
         globals()['cols' + str(i)] = st.columns(tlst)
         for j in range(len(tlst)):
-            strContent=build_tile_v2(str(i) + '-' + str(j))
+            strContent=build_tile(str(i) + '-' + str(j))
             globals()['cols' + str(i)][j].markdown(strContent, unsafe_allow_html=True)
             #globals()['cols' + str(i)][j].markdown(":orange-badge[Demo]")
         #build_tile()        build_tile_v2
         #st.markdown(":orange-badge[Demo]")
 
-
-def build_tile(oCell,strCaption="Caption Tile"):
-    oCell=st.markdown(":orange-badge[{strCaption}]")
-
-def build_tile_v2(name):
+def build_tile(name="Caption Tile",image_url="",level=1,stars=0):
     strHtml='<span>'
     strHtml=strHtml+'<table>'
     strHtml=strHtml+'<tr>'
-    strHtml=strHtml+'    <td rowspan=3>image</td>'
-    strHtml=strHtml+'    <td><b>'+name+'</b></td>'
+    strHtml=strHtml+'    <td rowspan=4><img src="'+image_url+'" width="80px"></td>'
+    strHtml=strHtml+'    <td colspan=2><b>'+name+'</b></td>'
     strHtml=strHtml+'</tr>'
     strHtml=strHtml+'<tr>'
-    strHtml=strHtml+'    <td>Cell'
-    strHtml=strHtml+'    </td>'
+    strHtml=strHtml+'    <td>Level</td><td>'+level+'</td>'
     strHtml=strHtml+'</tr>'
     strHtml=strHtml+'<tr>'
-    strHtml=strHtml+'    <td>Another Cell'
-    strHtml=strHtml+'    </td>'
+    strHtml=strHtml+'    <td>Stars</td><td>'+stars+'</td>'
+    strHtml=strHtml+'</tr>'
+    strHtml=strHtml+'<tr>'
+    strHtml=strHtml+'    <td>Cell</td><td>Cell</td>'
     strHtml=strHtml+'</tr>'
     strHtml=strHtml+'</table></span>'
     return strHtml
